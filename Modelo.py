dcb5980f59890295440d37a94462d5e0afb123a6
Carolina Serna V. <csernavanegas32@gmail.com>
2025-05-18 21:56:06 -0500

    Update Modelo.py
    Agregue comentarios

--- Modelo.py
+++ Modelo.py
@@ -1,11 +1,17 @@
-import cv2 as cv, numpy as np, os, pickle, tensorflow as tf
+import cv2 as cv
+import numpy as np
+import os
+import pickle
+import tensorflow as tf
 
-ruta_train = "./data/train" # Cambiar por la ruta de la carpeta train del dataset
-ruta_val = "./data/val" # Cambiar por la ruta de la carpeta val del dataset
+ruta_train = "./data/train" # Carpeta con imágenes de entrenamiento
+ruta_val = "./data/val" # Carpeta con imágenes de validacion
 
+# Categorias del dataset con los nombres de las subcarpetas
 categorias = ['circle', 'kite', 'parallelogram', 'rectangle', 'rhombus', 'square', 'trapezoid', 'triangle']
 etiqueta = {cat: i for i, cat in enumerate(categorias)}
 
+# Función para cargar y preprocesar imágenes
 def cargar_datos(ruta_base, categorias, etiqueta):
     datos_x, datos_y = [], []
     for categoria in categorias:
@@ -22,9 +28,11 @@ def cargar_datos(ruta_base, categorias, etiqueta):
             datos_y.append(etiqueta[categoria])
     return np.array(datos_x), np.array(datos_y)
 
+# Carga de datos
 x_train, y_train = cargar_datos(ruta_train, categorias, etiqueta)
 x_val, y_val = cargar_datos(ruta_val, categorias, etiqueta)
 
+# Definición del modelo CNN
 Conv_modelo = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(80, 80, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
@@ -38,6 +46,7 @@ Conv_modelo = tf.keras.models.Sequential([
     tf.keras.layers.Dense(len(categorias), activation='softmax')
 ])
 
+# Compilación y entrenamiento
 Conv_modelo.compile(optimizer='adam',
                     loss='sparse_categorical_crossentropy',
                     metrics=['accuracy'])
@@ -47,6 +56,7 @@ historial = Conv_modelo.fit(x_train, y_train,
                             batch_size=32,
                             validation_data=(x_val, y_val))
 
+#  Guardado del modelo y su historial
 Conv_modelo.save("modelo_formas.h5") #? Guardar el Modelo
 
 with open("historial_entrenamiento.pkl", "wb") as f: #? Guardar el historial del modelo
